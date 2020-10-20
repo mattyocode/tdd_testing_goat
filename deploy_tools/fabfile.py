d42838e9579453a24e7771a3a8e6a04ab387382f
mattyocode <matthewoliver@live.co.uk>
2020-10-20 12:40:44 +0100

    Added env.port

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -7,7 +7,7 @@ from fabric.api import cd, env, local, run
 REPO_URL = 'https://github.com/mattyocode/tdd_testing_goat.git'
 env.user = 'ubuntu'
 env.host = ['ec2-18-134-149-40.eu-west-2.compute.amazonaws.com']
-env.port
+env.port = 22
 env.key_filename = ['/Users/m.oliver/Desktop/Python/python-tdd-book/tdd-book-key.pem']
 
 def deploy():

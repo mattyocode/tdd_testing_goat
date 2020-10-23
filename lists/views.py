20ef96bfdf8a551941b79d9694d16db3b424355d
mattyocode <matthewoliver@live.co.uk>
2020-10-23 09:45:08 +0100

    Using new form in homepage, simplified tests. FT not passing

--- lists/views.py
+++ lists/views.py
@@ -3,10 +3,23 @@ from django.http import HttpResponse
 from django.core.exceptions import ValidationError
 
 from lists.models import Item, List
+from lists.forms import ItemForm
 
 # Create your views here.
 def home_page(request):
-    return render(request, 'lists/home.html')
+    return render(request, 'lists/home.html', {'form': ItemForm()})
+
+def new_list(request):
+    list_ = List.objects.create()
+    item = Item(text=request.POST['item_text'], list=list_)
+    try:
+        item.full_clean()
+        item.save()
+    except ValidationError:
+        list_.delete()
+        error = "You can't have an empty list item"
+        return render(request, 'lists/home.html', {"error": error})
+    return redirect(list_)
 
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
@@ -22,15 +35,3 @@ def view_list(request, list_id):
             error = "You can't have an empty list item"
 
     return render(request, 'lists/list.html', {'list': list_, 'error': error})
-
-def new_list(request):
-    list_ = List.objects.create()
-    item = Item(text=request.POST['item_text'], list=list_)
-    try:
-        item.full_clean()
-        item.save()
-    except ValidationError:
-        list_.delete()
-        error = "You can't have an empty list item"
-        return render(request, 'lists/home.html', {"error": error})
-    return redirect(list_)

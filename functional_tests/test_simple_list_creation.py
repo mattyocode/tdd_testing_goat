91ec1e5e8fe40a3b9937337d6c67d434c45777c1
mattyocode <matthewoliver@live.co.uk>
2020-10-23 10:09:48 +0100

    Renamed all input ids and names. FT still not passing

--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -18,7 +18,7 @@ class NewVisitorTest(FunctionalTest):
         self.assertIn('To-Do', header_text)
 
         # User is invited to enter a to-do list item immediately
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.get_item_input_box()
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
             'Enter a to-do item'
@@ -34,7 +34,7 @@ class NewVisitorTest(FunctionalTest):
 
         # There is still a text box inviting her to add another item.
         # User enters "Re-string guitar"
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.get_item_input_box()
         inputbox.send_keys('Re-string guitar')
         inputbox.send_keys(Keys.ENTER)
 
@@ -52,7 +52,7 @@ class NewVisitorTest(FunctionalTest):
     def test_multiple_users_can_start_lists_at_different_urls(self):
         # Edith starts a new to-do list
         self.browser.get(self.live_server_url)
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.get_item_input_box()
         inputbox.send_keys('Buy guitar strings')
         inputbox.send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy guitar strings')
@@ -73,7 +73,7 @@ class NewVisitorTest(FunctionalTest):
         self.assertNotIn('Re-string guitar', page_text)
 
         # Francis starts a new list by entering a new item.
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.get_item_input_box()
         inputbox.send_keys('Buy milk')
         inputbox.send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy milk')

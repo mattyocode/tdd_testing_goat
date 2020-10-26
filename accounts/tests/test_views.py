a73f939355825430dd5ce179154d724d2652ae69
mattyocode <matthewoliver@live.co.uk>
2020-10-26 22:24:39 +0000

    Unit tests passing. FT failing on no log out element

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -38,19 +38,6 @@ class SendLoginEmailViewTest(TestCase):
         )
         self.assertEqual(message.tags, "success")
 
-class LoginViewTest(TestCase):
-
-    def test_redirects_to_homepage(self):
-        response = self.client.get('/accounts/login?token=abc123')
-        self.assertRedirects(response, '/')
-
-    def test_creates_token_associated_with_email(self):
-        self.client.post('/accounts/send_login_email', data={
-            'email': 'user@example.com'
-        })
-        token = Token.objects.first()
-        self.assertEqual(token.email, 'user@example.com')
-
     @patch('accounts.views.send_mail')
     def test_sends_link_to_login_using_token_uid(self, mock_send_mail):
         self.client.post('/accounts/send_login_email', data={
@@ -62,10 +49,37 @@ class LoginViewTest(TestCase):
         (subject, body, from_email, to_list), kwards = mock_send_mail.call_args
         self.assertIn(expected_url, body)
 
-    @patch('accounts.views.auth')
+    def test_creates_token_associated_with_email(self):
+        self.client.post('/accounts/send_login_email', data={
+            'email': 'user@example.com'
+        })
+        token = Token.objects.first()
+        self.assertEqual(token.email, 'user@example.com')
+
+
+@patch('accounts.views.auth')
+class LoginViewTest(TestCase):
+
+    def test_redirects_to_homepage(self, mock_auth):
+        response = self.client.get('/accounts/login?token=abc123')
+        self.assertRedirects(response, '/')
+
     def test_calls_authenticate_with_uid_from_get_request(self, mock_auth):
         self.client.get('/accounts/login?token=abcd123')
         self.assertEqual(
             mock_auth.authenticate.call_args,
             call(uid='abcd123')
         )
+
+    def test_calls_auth_login_with_user_if_there_is_one(self, mock_auth):
+        response = self.client.get('/accounts/login?token=abcd123')
+        self.assertEqual(
+            mock_auth.login.call_args,
+            call(response.wsgi_request,
+            mock_auth.authenticate.return_value)
+        )
+
+    def test_does_not_login_if_user_is_not_authenticated(self, mock_auth):
+        mock_auth.authenticate.return_value = None
+        self.client.get('/accounts/login?token=abcd123')
+        self.assertEqual(mock_auth.login.called, False)

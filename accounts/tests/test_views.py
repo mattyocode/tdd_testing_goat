c4c81afa8eb1fc477e1fe81a4d53802ac5472577
mattyocode <matthewoliver@live.co.uk>
2020-10-26 21:27:18 +0000

    Login view authenticates through token uid

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from unittest.mock import patch
+from unittest.mock import patch, call
 
 import accounts.views
 from accounts.models import Token
@@ -61,3 +61,11 @@ class LoginViewTest(TestCase):
         expected_url = f'http://testserver/accounts/login?token={token.uid}'
         (subject, body, from_email, to_list), kwards = mock_send_mail.call_args
         self.assertIn(expected_url, body)
+
+    @patch('accounts.views.auth')
+    def test_calls_authenticate_with_uid_from_get_request(self, mock_auth):
+        self.client.get('/accounts/login?token=abcd123')
+        self.assertEqual(
+            mock_auth.authenticate.call_args,
+            call(uid='abcd123')
+        )
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,7 @@
 from django.core.mail import send_mail
 from django.core.urlresolvers import reverse
 from django.shortcuts import redirect
-from django.contrib import messages
+from django.contrib import auth, messages
 
 from accounts.models import Token
 
@@ -26,4 +26,5 @@ def send_login_email(request):
     return redirect('/')
 
 def login(request):
+    auth.authenticate(uid=request.GET.get('token'))
     return redirect('/')

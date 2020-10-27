122d5967268d978be29b711d1538da9fa5e378f1
mattyocode <matthewoliver@live.co.uk>
2020-10-27 21:54:36 +0000

    Added custom passworldless auth backend + custom user model

--- superlists/settings.py
+++ superlists/settings.py
@@ -132,3 +132,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'mattofcourse@gmail.com'
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+EMAIL_PORT = 465
+EMAIL_USE_TLS = True
